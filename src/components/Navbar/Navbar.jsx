0ae42b67b558b976eb60372284995b912d22edaf
Tato López <stefanolopez_1992@hotmail.com>
2022-11-24 21:47:23 -0300

    PreEntrega1 López

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,13 +3,24 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget/CartWidget';
 import "./Navbar.css";
+import logo from "./logo.png";
+
 
 function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" className='navbarra' variant="light" >
       <Container>
-        <Navbar.Brand href="#" className='nav__logo'>Tienda Aterrizar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Brand href="#" className='nav__logo'>
+          <img
+              alt=""
+              src={logo}
+              width="auto"
+              height="50"
+              className="d-inline-block align-top"
+            />{' '}
+            Tienda Aterrizar
+            </Navbar.Brand>      
+         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto ">
             <Nav.Link href="#" className='nav__link'>New Arrival</Nav.Link>
